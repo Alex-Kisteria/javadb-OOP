1fcb80daceddb33099b9905d859f92fb4027c367
Alexus <124175680+Alex-Kisteria@users.noreply.github.com>
2024-04-15 17:58:30 +0800

    Add files via upload

---
+++ src/main/java/com/example/csit228f2_2/MySQLConnection.java
@@ -0,0 +1,31 @@
+package com.example.csit228f2_2;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/javadb";
+    private static final String USER = "delta";
+    private static final String PASSWORD = "123";
+
+    public static Connection getConnection(){
+        Connection c = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to the Database!");
+        } catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+        return c;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        getConnection();
+    }
+}
+
---
+++ src/main/java/com/example/csit228f2_2/User.java
@@ -0,0 +1,17 @@
+package com.example.csit228f2_2;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class User {
+    String username;
+    String email;
+    String password;
+    String css;
+
+    public User(String username, String password) throws MalformedURLException {
+        this.username = username;
+        this.password = password;
+        css = username + ".css";
+    }
+}
